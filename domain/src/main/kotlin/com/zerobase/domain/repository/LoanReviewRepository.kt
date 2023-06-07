9a41782f3378a42d39dcc6f77c331b7c454af255
biber <dlehdxo2@gmail.com>
2023-06-07 20:27:06 +0900

    ☔️ : 설정 및 환경 테스트

--- domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
+++ domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
@@ -3,4 +3,5 @@ package com.zerobase.domain.repository
 import com.zerobase.domain.domain.LoanReview
 import org.springframework.data.jpa.repository.JpaRepository
 
+
 interface LoanReviewRepository : JpaRepository<LoanReview, Long>
