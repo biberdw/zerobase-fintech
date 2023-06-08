a2308da42fe9dc202a3dec8024719df588c8b872
biber <dlehdxo2@gmail.com>
2023-06-08 16:25:59 +0900

    🚀 : 예외 처리

--- domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
+++ domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 
 interface LoanReviewRepository : JpaRepository<LoanReview, Long> {
-    fun findByUserKey(userKey: String): LoanReview
+    fun findByUserKey(userKey: String): LoanReview?
 }
