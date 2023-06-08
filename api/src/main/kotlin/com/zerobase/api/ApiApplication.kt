e6ae89b9cd364f0fcf1a966b7ee02ebc50e33da3
biber <dlehdxo2@gmail.com>
2023-06-08 19:13:29 +0900

    🚀 : 카프카 Sender

--- api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
+++ api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
@@ -3,13 +3,13 @@ package com.zerobase.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EntityScan(basePackages = ["com.zerobase.domain"])
+@ComponentScan(basePackages = ["com.zerobase"])
 class ApiApplication
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
 }
--- api/src/main/kotlin/com/zerobase/api/loan/request/LoanRequestServiceImpl.kt
+++ api/src/main/kotlin/com/zerobase/api/loan/request/LoanRequestServiceImpl.kt
@@ -3,13 +3,16 @@ package com.zerobase.api.loan.request
 import com.zerobase.api.loan.GenerateKey
 import com.zerobase.api.loan.encrypt.EncryptComponent
 import com.zerobase.domain.repository.UserInfoRepository
+import com.zerobase.kafka.enum.KafkaTopic
+import com.zerobase.kafka.producer.LoanRequestSender
 import org.springframework.stereotype.Service
 
 @Service
 class LoanRequestServiceImpl(
     private val generateKey: GenerateKey,
     private val userInfoRepository: UserInfoRepository,
-    private val encryptComponent: EncryptComponent
+    private val encryptComponent: EncryptComponent,
+    private val loanRequestSender: LoanRequestSender
 
 ) : LoanRequestService {
     override fun loanRequestMain(
@@ -20,16 +23,23 @@ class LoanRequestServiceImpl(
 
         loanRequestInputDto.userRegistrationNumber = encryptComponent.encryptString(loanRequestInputDto.userRegistrationNumber)
 
+        val userInfoDto = loanRequestInputDto.toUserInfoDto(userKey)
+
         saveUserInfo(loanRequestInputDto.toUserInfoDto(userKey))
 
-        loanRequestReview(userKey)
+        loanRequestReview(userInfoDto)
+
         return LoanRequestDto.LoanRequestResponseDto(userKey)
     }
 
     override fun saveUserInfo(userInfoDto: UserInfoDto) =
         userInfoRepository.save(userInfoDto.toEntity())
 
-    override fun loanRequestReview(userKey: String) {
+    override fun loanRequestReview(userInfoDto: UserInfoDto) {
+        loanRequestSender.sendMessage(
+            KafkaTopic.LOAN_REQUEST,
+            userInfoDto.toLoanRequestKafkaDto()
+        )
     }
 
 }
